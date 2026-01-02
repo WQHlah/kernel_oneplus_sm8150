150cd77b8afce3cadf299fa925a2aa507be944ce
lihaoyu <109925622+mingmingzhichu@users.noreply.github.com>
2026-01-02 07:55:17 +0000

    fix

--- net/ipv4/tcp_bbr.c
+++ net/ipv4/tcp_bbr.c
@@ -432,21 +432,8 @@ static u32 bbr_inflight(struct sock *sk, u32 bw, int gain)
  */
 static u32 bbr_packets_in_net_at_edt(struct sock *sk, u32 inflight_now)
 {
-	struct tcp_sock *tp = tcp_sk(sk);
-	struct bbr *bbr = inet_csk_ca(sk);
-	u64 now_ns, edt_ns, interval_us;
-	u32 interval_delivered, inflight_at_edt;
-
-	now_ns = tp->tcp_clock_cache;
-	edt_ns = max(tp->tcp_wstamp_ns, now_ns);
-	interval_us = div_u64(edt_ns - now_ns, NSEC_PER_USEC);
-	interval_delivered = (u64)bbr_bw(sk) * interval_us >> BW_SCALE;
-	inflight_at_edt = inflight_now;
-	if (bbr->pacing_gain > BBR_UNIT)              /* increasing inflight */
-		inflight_at_edt += bbr_tso_segs_goal(sk);  /* include EDT skb */
-	if (interval_delivered >= inflight_at_edt)
-		return 0;
-	return inflight_at_edt - interval_delivered;
+	/* EDT not supported in this kernel version, return 0 */
+	return 0;
 }
 
 /* Find the cwnd increment based on estimate of ack aggregation */
